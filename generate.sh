cace2835f0218e057bcfeb6e965ec2bda2f029ca
Javier Marcos <javiermarcosfer@gmail.com>
2025-03-13 07:15:28 -0400

    Reworked generation script

--- generate.sh
+++ generate.sh
@@ -1,111 +1,20 @@
 #!/bin/bash
 
-SUBFOLDER="extra"
+function add_prefix {
+	echo -e "// === $1 ==="
+	for FILE in *; do
+		if [[ "$FILE" == $2 ]]; then
+			echo -e "addfile \"addons/$FILE\""
+		fi
+	done
+}
 
-# The first write into the loadmods.cfg file is done with > rather than >> to empty the file if it already exists
-# Rather than append to a previously generated loadmods.cfg
+add_prefix "PRIORITY" "A?_D*" > .coreMods.cfg
+add_prefix "CHARACTERS" "DC*_*"  >> .coreMods.cfg
+add_prefix "MAPS" "DR*_*"  >> .coreMods.cfg
+add_prefix "FOLLOWERS" "DF*_*"  >> .coreMods.cfg
+add_prefix "LUA" "DL*_*"  >> .coreMods.cfg
+add_prefix "SOC" "D_*_*"  >> .coreMods.cfg
+add_prefix "MISC." "Z_D*"  >> .coreMods.cfg
 
-# Priority (A_)
-echo "// As a rule of thumb, complex addons need to be loaded first
-
-// Pizza Characters include any chars related to Pizza Tower/McPigs personal projects
-// Server characters include any other OCs from Doc Oc
-// SEGA Characters include any characters from SEGA, as well as fanmade OCs/AUs
-// Misc Characters is for everything else, including shitpost characters
-
-// Priority (A_)" > .loadmods.cfg
-for FILE in *; do
-	if [[ "$FILE" == A?_D* ]]; then
-		echo -e "addfile \"addons/$FILE\"" >> .loadmods.cfg
-	fi
-done
-for FILE in "${SUBFOLDER}"/*; do
-	if [[ "$FILE" == ${SUBFOLDER}/A?_D* ]]; then
-		echo -e "// addfile \"addons/$FILE\"" >> .loadmods.cfg
-	fi
-done
-
-# Characters (C)
-echo "
-// Characters (C)" >> .loadmods.cfg
-for FILE in *; do
-	if [[ "$FILE" == "DC"* ]]; then
-		echo -e "addfile \"addons/$FILE\"" >> .loadmods.cfg
-	fi
-done
-for FILE in "${SUBFOLDER}"/*; do
-    if [[ "$FILE" == "${SUBFOLDER}/DC"* ]]; then
-        echo -e "// addfile \"addons/$FILE\"" >> .loadmods.cfg
-    fi
-done
-
-# Maps (M)
-echo "
-// Maps (R)" >> .loadmods.cfg
-for FILE in *; do
-	if [[ "$FILE" == "DR"* ]]; then
-		echo -e "addfile \"addons/$FILE\"" >> .loadmods.cfg
-	fi
-done
-for FILE in "${SUBFOLDER}"/*; do
-    if [[ "$FILE" == "${SUBFOLDER}/DR"* ]]; then
-        echo -e "// addfile \"addons/$FILE\"" >> .loadmods.cfg
-    fi
-done
-
-# Followers (F)
-echo "
-// Followers (F)" >> .loadmods.cfg
-for FILE in *; do
-	if [[ "$FILE" == "DF"* ]]; then
-		echo -e "addfile \"addons/$FILE\"" >> .loadmods.cfg
-	fi
-done
-for FILE in "${SUBFOLDER}"/*; do
-    if [[ "$FILE" == "${SUBFOLDER}/DF"* ]]; then
-        echo -e "// addfile \"addons/$FILE\"" >> .loadmods.cfg
-    fi
-done
-
-
-# LUA (L)
-echo "
-// LUA (L)" >> .loadmods.cfg
-for FILE in *; do
-	if [[ "$FILE" == "DL"* ]]; then
-		echo -e "addfile \"addons/$FILE\"" >> .loadmods.cfg
-	fi
-done
-for FILE in "${SUBFOLDER}"/*; do
-    if [[ "$FILE" == "${SUBFOLDER}/DL"* ]]; then
-        echo -e "// addfile \"addons/$FILE\"" >> .loadmods.cfg
-    fi
-done
-
-# SOC (D)
-echo "
-// SOC (D)" >> .loadmods.cfg
-for FILE in *; do
-	if [[ "$FILE" == "D_"* ]]; then
-		echo -e "addfile \"addons/$FILE\"" >> .loadmods.cfg
-	fi
-done
-for FILE in "${SUBFOLDER}"/*; do
-    if [[ "$FILE" == "${SUBFOLDER}/D_"* ]]; then
-        echo -e "// addfile \"addons/$FILE\"" >> .loadmods.cfg
-    fi
-done
-
-# Miscellaneous (Z_)
-echo "
-// Miscellaneous (Z_)" >> .loadmods.cfg
-for FILE in *; do
-	if [[ "$FILE" == "Z_D"* ]]; then
-		echo -e "addfile \"addons/$FILE\"" >> .loadmods.cfg
-	fi
-done
-for FILE in "${SUBFOLDER}"/*; do
-    if [[ "$FILE" == "${SUBFOLDER}/Z_D"* ]]; then
-        echo -e "// addfile \"addons/$FILE\"" >> .loadmods.cfg
-    fi
-done
+add_prefix "EXTRA CHARACTERS" "EX_D*_*" > .extraCharacters.cfg
