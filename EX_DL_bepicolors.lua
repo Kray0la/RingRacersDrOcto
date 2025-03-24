8530476d015fa3de3c4bdc050e276920acd8ecfc
Javier Marcos <javiermarcosfer@gmail.com>
2025-03-23 20:49:18 -0400

    Added Somari

---
+++ EX_DL_bepicolors.lua
@@ -0,0 +1,12 @@
+freeslot(
+	"SKINCOLOR_SOMARI"
+	)
+
+skincolors[SKINCOLOR_SOMARI] = {
+    name = "putoelquelolea",
+    ramp = {34,34,34,34,34,34,39,39,155,155,155,155,155,71,71,71},
+    invcolor = SKINCOLOR_WHITE,
+    invshade = 15,
+    chatcolor = V_REDMAP,
+    accessible = false
+}
