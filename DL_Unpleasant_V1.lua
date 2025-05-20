a1519d39c2aaf5f2c4074f9ca7446a3fda084f56
Javier Marcos <javiermarcosfer@gmail.com>
2025-05-19 21:25:17 -0400

    Big update

---
+++ DL_Unpleasant_V1.lua
@@ -0,0 +1,12 @@
+freeslot(
+"SKINCOLOR_UNPLEASANT"
+)
+     
+skincolors[SKINCOLOR_UNPLEASANT] = {          
+    name = "Unpleasant",
+    ramp = {112,112,113,99,192,180,180,181,181,182,204,59,60,61,43,71
+},
+    invcolor = SKINCOLOR_VOMIT,
+    chatcolor = V_PINKMAP,
+    accessible = true
+}	
